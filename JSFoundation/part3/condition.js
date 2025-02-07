6a303c33ae38489b89709084392a3e7ba94d1bd7
Shrutika <shrutiwakodikar96@gmail.com>
2025-02-07 22:56:07 +0530

    primitive nonprimitive condition

---
+++ JSFoundation/part3/condition.js
@@ -0,0 +1,70 @@
+// ******************************
+// Checking if a number is greater than another number:
+// *********************************
+
+// let num1 = 5;
+// let num2 = 8;
+
+// console.log("hi");
+
+// if (num1 > num2) {
+//   console.log("num1 is greater than num2");
+// }
+// if (num1 < num2) {
+//   console.log("num2 is greater than num1");
+// }
+
+// console.log("Hello");
+
+// if (num1 > num2) {
+//   console.log("num1 is greater than num2");
+// } else {
+//   console.log("num1 is smaller than num2");
+// }
+
+// ******************************
+// Checking if string is equl to another string:
+
+// let username = "Chai";
+// let anotherUsername = "Tea";
+
+// if (username == anotherUsername) {
+//   console.log("SAME");
+// } else {
+//   console.log("NOT SAME");
+// }
+
+// Checking is variable is number of not:
+
+// let score = "44";
+// if (typeof score === "number") {
+//   console.log("data type is same");
+// } else {
+//   console.log("data type is not same");
+// }
+
+// Checking if a boolean value is truw or false
+
+// let isTeaReady = false;
+// if (isTeaReady) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
+
+// if (isTeaReady == true) {
+//   console.log("Yes tea is ready");
+// } else {
+//   console.log("Tea is not ready");
+// }
+
+// Checking is array is empty or not
+
+let arr = [];
+
+console.log(arr.length);
+if (arr.length === 0) {
+  console.log(" its empty");
+} else {
+  console.log("its not empty");
+}
