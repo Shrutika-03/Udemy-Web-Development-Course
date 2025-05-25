a10123c6df21b59223c0947d3f7f646afbf4aaae
Shrutika <shrutiwakodikar96@gmail.com>
2025-05-25 20:20:04 +0530

    Quiz-application

---
+++ Quiz-application/script.js
@@ -0,0 +1,84 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const startBtn = document.getElementById("start-btn");
+  const nextBtn = document.getElementById("next-btn");
+  const restartBtn = document.getElementById("restart-btn");
+
+  const questionContainer = document.getElementById("question-container");
+  const questionText = document.getElementById("question-text");
+  const choicesList = document.getElementById("choices-list");
+  const resultContainer = document.getElementById("result-container");
+  const scoreDisplay = document.getElementById("score");
+
+  const questions = [
+    {
+      question: "What is the capital of France",
+      choices: ["Paris", "London", "Berlin", "Madrid"],
+      answer: "Paris",
+    },
+    {
+      question: "What is the capital of Germany",
+      choices: ["Paris", "London", "Berlin", "Madrid"],
+      answer: "Berlin",
+    },
+    {
+      question: "What is the capital of America",
+      choices: ["Washington DC", "London", "Berlin", "Madrid"],
+      answer: "Berlin",
+    },
+  ];
+
+  let currentQuestionIndex = 0;
+  let score = 0;
+
+  startBtn.addEventListener("click", startQuiz);
+
+  nextBtn.addEventListener("click", () => {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+      showQuestion();
+    } else {
+      showResult();
+    }
+  });
+
+  restartBtn.addEventListener("click", () => {
+    currentQuestionIndex = 0;
+    score = 0;
+    resultContainer.classList.add("hidden");
+    startQuiz();
+  });
+
+  function startQuiz() {
+    startBtn.classList.add("hidden");
+    resultContainer.classList.add("hidden");
+    questionContainer.classList.remove("hidden");
+    showQuestion();
+  }
+
+  function showQuestion() {
+    nextBtn.classList.add("hidden");
+    questionText.textContent = questions[currentQuestionIndex].question;
+    choicesList.innerHTML = ""; //clear previous choices
+    questions[currentQuestionIndex].choices.forEach((choice) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = choice;
+      listItem.addEventListener("click", () => selectAnswer(choice));
+      choicesList.appendChild(listItem);
+    });
+  }
+
+  function selectAnswer(choice) {
+    const correctAnswer = questions[currentQuestionIndex].answer;
+
+    if (choice === correctAnswer) {
+      score++;
+    }
+    nextBtn.classList.remove("hidden");
+  }
+
+  function showResult() {
+    questionContainer.classList.add("hidden");
+    resultContainer.classList.remove("hidden");
+    scoreDisplay.textContent = `${score} out of ${questions.length}`;
+  }
+});
